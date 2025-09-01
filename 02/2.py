504baace707ee1d08d37d8809b65ec99c90ae448
username <oleksandrkrasnokutskyi@MacBook-Air-Oleksandr.local>
2025-09-01 19:34:57 +0300

    Implementing 2nd and 3rd homework

--- 02/2.py
+++ 02/2.py
@@ -1,7 +1,40 @@
 import sys
-#import math
-from math import pi
-print('Hello world')
-print(sys.argv[0])
-#print(sys.argv[1])
-print(pi )
+import math
+#TASK 1 HW
+Enter_your_number =int(input("Enter a four digit number:"))
+x=1000
+left, right = divmod(Enter_your_number, x)
+print(left)
+
+y=100
+step1,step2=divmod(right,y)
+print(step1)
+
+z=10
+step3,step4=divmod(step2,z)
+print(step3)
+
+v=1
+step5,step6=divmod(step4,v)
+print(step5)
+
+# v=10
+# step5,step6=divmod(right,v)
+# print(step6)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- 02/3.py
+++ 02/3.py
@@ -1,5 +1,14 @@
-from langdetect import detect
+import sys
+import math
+#TASK 2 HW
+Enter_number=int(input("Enter your number:"))
 
-#print(detect("War doesn't show who's right, just who's left."))
-lan1 = detect("War doesn't show who's right, just who's left.")
-print(lan1)
+divider=10
+
+left, right = divmod(Enter_number, divider)
+step1, step2 = divmod(left, divider)
+step3, step4 = divmod(step1, divider)
+step5, step6 = divmod(step3, divider)
+step7, step8 = divmod(step5, divider)
+
+print(right*10000 + step2*1000 + step4 * 100 + step6*10 + step8)
