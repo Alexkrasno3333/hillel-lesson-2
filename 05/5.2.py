e7d60d90ebbbec1cd2dd3e444a84359ce92c3a38
Alexkrasno3333 <goverla123456789@gmail.com>
2025-09-15 17:55:00 +0300

    change hw 5.2

--- 05/5.2.py
+++ 05/5.2.py
@@ -3,49 +3,28 @@ while True:
     num2 = int(input("ENTER YOUR NUMBER :"))
     plus = input("ENTER YOUR + - * / :")
 
-    result1 = num1 + num2
-    result2 = num1 - num2
-    result3 = num1 * num2
-
     if plus == "+":
         print(num1 + num2)
-        if result1 == num1 + num2:
-            men = input("Y/N: ")
-            if men == "Y":
-                print("OK BRO DO AGAIN")
-            else:
-                break
 
     elif plus == "-":
         print(num1 - num2)
-        if result2 == num1 - num2:
-            men = input("Y/N: ")
-            if men == "Y":
-                print("OK BRO DO AGAIN")
-            else:
-                break
 
     elif plus == "*":
         print(num1 * num2)
-        if result3 == num1 * num2:
-            men = input("Y/N: ")
-            if men == "Y":
-                print("OK BRO DO AGAIN")
-            else:
-                break
 
     elif plus == "/":
         if num2 == 0:
             print("YOU CANT DO THIS")
         else:
-            result4 = num1 / num2
-            print(result4)
-            if result4 == num1 / num2:
-                men = input("Y/N: ")
-                if men == "Y":
-                    print("OK BRO DO AGAIN")
-                else:
-                    break
+            print(num1/num2)
+
+    men = input("yes/no ").lower()
+    if men in ("y","yes"):
+        print("OK BRO DO AGAIN")
+    else:
+        break
+
+
 
 
 
