2724618b3b2060417522c100be04b953a86dfcfd
Alexkrasno3333 <goverla123456789@gmail.com>
2025-09-15 18:23:07 +0300

    исправил ошибки вроде рабоатет

--- 05/5.1.py
+++ 05/5.1.py
@@ -4,54 +4,45 @@ import string
 print_text = input("Enter your text: ")
 
 underscores = 0
-key = 0
 has_upper = 0
 invalid_char = 0
 
-for char in print_text:
-
-    if char.isupper():
-        has_upper = 1
-
-    if char in string.punctuation:
-        if char != "_":
-            invalid_char = 1
-    if char == " ":
-        invalid_char = 1
-
-    if char == "_":
-        underscores += 1
-
-if has_upper:
+if not print_text:
     print("FALSE")
 
-elif print_text[0].isdigit():
-    print("FALSE")
+elif print_text == "_":
+    print("TRUE")
 
-elif underscores > 1:
-    print("FALSE")
-
-elif invalid_char:
-    print("FALSE")
-
-elif print_text in keyword.kwlist:
+elif all(ch == "_" for ch in print_text):
     print("FALSE")
 
 else:
-    print("TRUE")
+    for char in print_text:
+        if char.isupper():
+            has_upper = 1
 
+        if char in string.punctuation and char != "_":
+            invalid_char = 1
+        if char == " ":
+            invalid_char = 1
 
+        if char == "_":
+            underscores += 1
 
+    if has_upper:
+        print("FALSE")
 
+    elif print_text[0].isdigit():
+        print("FALSE")
 
+    elif invalid_char:
+        print("FALSE")
 
+    elif print_text in keyword.kwlist:
+        print("FALSE")
 
-
-
-
-
-
-
+    else:
+        print("TRUE")
 
 
 
